26758a1ab5ef4215b3e423e7254ec550e85e321a
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-06 08:06:40 +0530

    added: Calculated Calculate Wages till

--- EmpWage.java
+++ EmpWage.java
@@ -7,17 +7,17 @@ public class EmpWage {
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Program");
+		// variables
+		int totalEmpWage = 0, totalWorkingDays = 0, totalEmpHrs = 0;
 
-		//variables
-		int totalEmpWage = 0;
-		int empHrs = 0;
-		int empWage = 0;
-
-		for (int days = 0; days < NUM_OF_WORKING_DAYS; days++) {
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			int empHrs = 0;
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case IS_FULL_TIME:
@@ -29,11 +29,14 @@ public class EmpWage {
 			default:
 				empHrs = 0;
 			}
-			empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpHrs = totalEmpHrs + empHrs;
+			int empWage = empHrs * EMP_RATE_PER_HOUR;
 			totalEmpWage = totalEmpWage + empWage;
 			System.out.println("Emp Wage: " + empWage);
 		}
 		System.out.println("Total Employee Wage: " + totalEmpWage);
+
 	}
+
 
 }
