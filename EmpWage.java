e527e6e1497f7aa09611d9cd087f896c1c8b6475
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-06 06:22:36 +0530

    added: Attendance check of an employee

--- EmpWage.java
+++ EmpWage.java
@@ -1,10 +1,23 @@
 package com.bridgelabz;
 
 public class EmpWage {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		
+		EmpWage empwage = new EmpWage();
+		empwage.attendanceCheck();
+
+	}
+
+	private void attendanceCheck() {
+
+		int isPresent = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isPresent) {
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is absent");
 		}
+	}
 
 }
