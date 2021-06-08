30bf0fcdfd909d293f1bb5198056a256dfd9d831
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-08 16:28:24 +0530

    refactored: Ability to manage Employee Wage in multiple company using interface approach

--- EmpWageBuilderArray.java
+++ EmpWageBuilderArray.java
@@ -1,60 +1,61 @@
 package com.bridgelabz;
 
-public class EmpWageBuilderArray {
+public class EmpWageBuilderArray implements IfComputeEmpWage {
 
 	// constants
-		public static final int IS_PART_TIME = 1;
-		public static final int IS_FULL_TIME = 2;
-		
-		private int numOfCompany = 0;
-		private CompanyEmpWage[] companyEmpWageArray;
-		
-		public EmpWageBuilderArray() {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+
+	private int numOfCompany = 0;
+	private CompanyEmpWage[] companyEmpWageArray;
+
+	public EmpWageBuilderArray() {
 		companyEmpWageArray = new CompanyEmpWage[5];
+	}
+
+	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays,
+				maxHoursPerMonth);
+
+		numOfCompany++;
+	}
+
+	public void computeEmpWage() {
+		for (int i = 0; i < numOfCompany; i++) {
+			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
+			System.out.println(companyEmpWageArray[i]);
 		}
-		
-		private void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-			companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
-			
-			numOfCompany++;
-		}
-		
-		private void computeEmpWage() {
-			for(int i=0; i<numOfCompany; i++) {
-				companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
-				System.out.println(companyEmpWageArray[i]);
+	}
+
+	private int computeEmpWage(CompanyEmpWage companyEmpWage) {
+		// variables
+		int empHrs = 0, totalWorkingDays = 0, totalEmpHrs = 0;
+		// computation
+		while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
 			}
+			totalEmpHrs = totalEmpHrs + empHrs;
+			System.out.println("DAY#: " + totalWorkingDays + " Emp Hr: " + empHrs);
 		}
-		
-		private int computeEmpWage(CompanyEmpWage companyEmpWage) {
-			// variables
-			int empHrs = 0, totalWorkingDays = 0, totalEmpHrs = 0;
-			// computation
-			while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays) {
-				totalWorkingDays++;
-				int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-				switch (empCheck) {
-				case IS_FULL_TIME:
-					empHrs = 8;
-					break;
-				case IS_PART_TIME:
-					empHrs = 4;
-					break;
-				default:
-					empHrs = 0;
-				}
-				totalEmpHrs = totalEmpHrs + empHrs;
-				System.out.println("DAY#: " + totalWorkingDays + " Emp Hr: " + empHrs);
-			}
-				return totalEmpHrs *companyEmpWage.empRatePerHour;
-		}
-		
-		public static void main(String args[]) {
-			EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
-			empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
-			empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
-			empWageBuilder.computeEmpWage();
-			
-		}
-		
+		return totalEmpHrs * companyEmpWage.empRatePerHour;
+	}
+
+	public static void main(String args[]) {
+		EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+		empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+		empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+		empWageBuilder.computeEmpWage();
+
+	}
+
 }
---
+++ IfComputeEmpWage.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public interface IfComputeEmpWage {
+	
+	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+	public void computeEmpWage();
+}
